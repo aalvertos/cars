8fed0080eddcd678a3e95d824cdb3ec2453aa00b
Alex ALvertos <alver104@mail.chapman.edu>
2018-11-28 10:48:25 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#cars.sh
+#alexAlvertos
+
+echo "Old cars database choose an option"
+echo "option 1: enter a car"
+echo "option 2: list the cars"
+echo "option 3: quit"
+read INPUT
+sep=":"
+
+while true; do
+      case input in
+'1')echo  option 1
+echo  " enter the model of car"
+echo  " enter the year of car" 
+echo  " enter the make of the car
+ 
+
